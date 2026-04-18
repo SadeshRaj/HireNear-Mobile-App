b711c19fc4e08ee08285044ba5862c8b21d33e01
SadeshRaj <sadeshrajapakshe@gmail.com>
2026-04-18 11:24:37 +0530

    Finetune mobile login

--- mobile/config.js
+++ mobile/config.js
@@ -1,8 +1,1 @@
-// config.js
-
-// Paste the IP address your friend's terminal spits out right here.
-// When their IP changes tomorrow, you ONLY change this one line.
-const LOCAL_IP = '192.168.8.140';
-const PORT = '5000';
-
 export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
