5882f37bd2e8a8108b2e7f9a29f42db54e9cabe8
kaveesha <kaveeshatharindi333@gmail.com>
2026-05-01 16:14:43 +0530

    addReview

--- mobile/config.js
+++ mobile/config.js
@@ -1,7 +1,5 @@
 // config.js — Single source of truth for the API URL
 // Set EXPO_PUBLIC_API_URL in mobile/.env to switch between emulator and physical device
 
-//export const API_BASE_URL =
-  //  process.env.EXPO_PUBLIC_API_URL || 'http://10.0.2.2:4000/api';
-
-export const API_BASE_URL = 'http://192.168.8.160:4000/api';
+export const API_BASE_URL =
+    process.env.EXPO_PUBLIC_API_URL || 'http://10.0.2.2:4000/api';
